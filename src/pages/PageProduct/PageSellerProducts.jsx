013dfdbdddb707beff4b5c76afefbabe56d40fea
AnhDieuTran <100272354+AnhDieuTran@users.noreply.github.com>
2024-05-23 19:35:45 +0700

    update store

--- src/pages/PageProduct/PageSellerProducts.jsx
+++ src/pages/PageProduct/PageSellerProducts.jsx
@@ -2,18 +2,21 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../../components/Loader/Loader";
 import NotFound from "../../components/NotFound/NotFound";
+import classname from "../../helpers/classJoiner";
 import Card from "../../components/Card/Card";
+import Img from "../../components/ImgWithContainer/ImgWithContainer";
 import styles from "./styles.module.css";
 import { getProfileStoreCreator } from "../../redux/actions/auth";
 import { getProductBySellerIdCreator } from "../../redux/actions/product";
-import logoShop from "../../assets/img/logoshop.png";
+import defaultAvatar from "../../assets/img/logoshop.png"; // Thay đổi đường dẫn tùy theo nơi lưu trữ ảnh mặc định
+import { API_URL } from "../../utils/environment";
 
 const PageSellerProducts = (props) => {
     const [isLoading, setIsLoading] = useState(true);
     const dispatch = useDispatch();
     const { dataGetProdBySelId } = useSelector((state) => state.product);
     const { sellerInfo, isGetStoreProfilePending } = useSelector((state) => state.auth);
-    
+
     useEffect(() => {
       const sellerId = props.match.params.id; 
       dispatch(getProfileStoreCreator(sellerId));
@@ -38,34 +41,49 @@ const PageSellerProducts = (props) => {
         props.history.push(`/product/detail/${id}`);
     };
 
-    const shop = sellerInfo[0];
+    const shop = sellerInfo.length > 0 ? sellerInfo[0] : {};
 
     return (
-        <div className={styles.body}>
-            {/* Seller Info Section */}
-            <div className={styles.user}>
-            <img src={shop.avatar || logoShop} alt="Seller Logo" />
+      <div className={styles.body}>
+          {/* Seller Info Section */}
+          <div className={styles.user}>
+            <Img
+              source={`${API_URL}${shop.avatar}`|| defaultAvatar}
+              containerStyle={styles.cardImgContainer}
+              imgStyle={styles.cardImg}
+            />
+            <div className={styles.store}>
                 <h1>{shop.store_name}</h1>
                 <p>{shop.store_desc}</p>
             </div>
-
-            <h1>Products Section</h1>
-            <div className={styles.productList}>
-                {dataGetProdBySelId.map((product) => (
-                    <Card
-                        key={product.id}
-                        id={product.id}
-                        image={product.images.length > 0 ? product.images[0] : ''}
-                        name={product.name} 
-                        price={product.price.toString()} 
-                        seller_name={shop.store_name}
-                        rate={product.rating || 0} 
-                        onClickProp={() => onClickHandler(product.id)}
-                    />
-                ))}
-            </div>
         </div>
-    );
+
+
+          <h1>Products Section</h1>
+          <div className="w-100">
+            <div
+              className={classname(
+                "row",
+                "no-gutters",
+                "d-flex flex-row"
+              )}
+            >
+              {dataGetProdBySelId.map((product) => (
+                      <Card
+                          key={product.id}
+                          id={product.id}
+                          image={product.images.length > 0 ? product.images[0] : ''}
+                          name={product.name} 
+                          price={product.price.toString()} 
+                          seller_name={shop.store_name}
+                          rate={product.rating || 0} 
+                          onClickProp={() => onClickHandler(product.id)}
+                      />
+                  ))}
+            </div>
+          </div>
+      </div>
+  );
 };
 
 export default PageSellerProducts;
